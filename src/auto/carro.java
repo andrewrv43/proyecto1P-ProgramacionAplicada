c17a6200c6a44ebea34e7038edf687ba2eea517a
Andrewrv43 <paul.rueda@hotmail.com>
2022-05-22 14:02:55 -0500

    carroAct

--- src/auto/carro.java
+++ src/auto/carro.java
@@ -1,5 +1,28 @@
 package auto;
 
 public class carro {
+	private String marca, modelo, color, placa, tapiceria,cjCambios;
+	private long kilometraje;
+	private int year,descuento;
+	private double precio;
+	
+	public carro(String marca, String modelo, String color, String placa, String tapiceria,
+			String cjCambios, long kilometraje,int year, int descuento,double precio) {
+		this.marca=marca;
+		this.modelo=modelo;
+		this.color=color;
+		this.placa=placa;
+		this.tapiceria=tapiceria;
+		this.cjCambios=cjCambios;
+		this.kilometraje=kilometraje;
+		this.year=year;
+		this.descuento=descuento;
+		this.precio=precio;
+	}
+	
+	public String getDatos() {
+		return "";
+	}
+	
 
 }
