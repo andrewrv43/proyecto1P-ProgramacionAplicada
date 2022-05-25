fd7df5ea647aab025b5655788f4736d2dfa1999f
Andrewrv43 <paul.rueda@hotmail.com>
2022-05-24 21:37:41 -0500

    nuloAprobado

--- src/Ventanas/ventana_registrar.java
+++ src/Ventanas/ventana_registrar.java
@@ -95,7 +95,7 @@ public class ventana_registrar extends JPanel {
 			@SuppressWarnings("deprecation")
 			public void actionPerformed(ActionEvent e) {
 				if(ingreso(in_usuario.getText(), in_password.getText())) {
-					
+
 				}else {
 					JOptionPane.showInputDialog(this, "Usuario o contraseña incorrecta");
 
@@ -113,30 +113,31 @@ public class ventana_registrar extends JPanel {
 	}
 	@SuppressWarnings("resource")
 	public boolean ingreso(String user, String pass) {
-		
+String b="";
 		BufferedReader br;
 		try {
 			br = new BufferedReader (new FileReader ("src/datos.txt"));
 			try {
-				while(br != null) {
-					String b = br.readLine();
+				do {
+				 b=br.readLine();
+					
 					String[] s1 = b.split(";");
-					System.out.println(s1[0] + " " + s1[1]);
-					if(s1[0].equals(user) && s1[1].equals(pass)) {
-						System.out.println("asd");
+					if(pass.equals(s1[1])&&user.equals(s1[0])) {
+						System.out.println("ACEPTADO");
 						return true;
 					}
-					System.out.println("no entrew");
-				}
+					//System.out.println(s1[0] + " " + s1[1]);
+				b=br.readLine();
+				}while(b != null);
 				return false;
-				
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				System.out.println("malo 1");
 			}
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch 
-System.out.println("malo");
+			System.out.println("malo");
 		}
 		return false;
 
