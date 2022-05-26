0b2dc4e566af65844c7702996a78cca82d367796
Andrewrv43 <paul.rueda@hotmail.com>
2022-05-25 19:31:10 -0500

    importar

--- src/Ventanas/Ventana_opciones.java
+++ src/Ventanas/Ventana_opciones.java
@@ -44,19 +44,19 @@ public class Ventana_opciones extends JPanel {
 	public JButton btnNewButton = new JButton("REGISTRAR");
 	public Ventana_opciones() {
 		setLayout(null);
-		
+		imp();
 		JPanel panel = new JPanel();
 		panel.setBorder(new MatteBorder(5, 3, 1, 1, (Color) new Color(0, 0, 0)));
 		panel.setBounds(21, 21, 425, 441);
 		add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblSajacar = new JLabel("SAJACAR");
 		lblSajacar.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSajacar.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 31));
 		lblSajacar.setBounds(116, 10, 178, 28);
 		panel.add(lblSajacar);
-		
+
 		JEditorPane dtrpnBienvenidoEstaEs = new JEditorPane();
 		dtrpnBienvenidoEstaEs.setEditable(false);
 		dtrpnBienvenidoEstaEs.setFont(new Font("Century Gothic", Font.ITALIC, 13));
@@ -65,20 +65,20 @@ public class Ventana_opciones extends JPanel {
 				+ " espero que disfrutes \r\ncon nosotros esta experiencia y recuerda... \r\nQuieres un sue\u00F1o en SAJACAR lo cumpliras.");
 		dtrpnBienvenidoEstaEs.setBounds(10, 60, 407, 82);
 		panel.add(dtrpnBienvenidoEstaEs);
-		
+
 		JLabel lblElijeUnaOpcin = new JLabel("Elije una opci\u00F3n:");
 		lblElijeUnaOpcin.setHorizontalAlignment(SwingConstants.LEFT);
 		lblElijeUnaOpcin.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 20));
 		lblElijeUnaOpcin.setBounds(10, 147, 178, 28);
 		panel.add(lblElijeUnaOpcin);
-		
-		
+
+
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				vr = new ventana_registrar(vo);
 				JFrame a = new JFrame();
-				
+
 				a.setBounds(50, 50, 650, 400);
 				a.getContentPane().add(vr);
 				a.setVisible(true);
@@ -90,7 +90,7 @@ public class Ventana_opciones extends JPanel {
 		btnNewButton.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 20));
 		btnNewButton.setBounds(20, 185, 178, 39);
 		panel.add(btnNewButton);
-		
+
 		JButton btnCatalogo = new JButton("CATALOGO");
 		btnCatalogo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -104,17 +104,17 @@ public class Ventana_opciones extends JPanel {
 		btnCatalogo.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 20));
 		btnCatalogo.setBounds(20, 251, 178, 39);
 		panel.add(btnCatalogo);
-		
+
 		JButton btnComprar = new JButton("COMPRAR");
 		btnComprar.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 20));
 		btnComprar.setBounds(20, 313, 178, 39);
 		panel.add(btnComprar);
-		
+
 		JButton btnVender = new JButton("VENDER");
 		btnVender.setFont(new Font("Century Gothic", Font.BOLD | Font.ITALIC, 20));
 		btnVender.setBounds(20, 375, 178, 39);
 		panel.add(btnVender);
-		
+
 		vo=this;
 	}
 	public void add(carro d) {
@@ -148,20 +148,20 @@ public class Ventana_opciones extends JPanel {
 		BufferedReader br;
 		try {
 			FileReader lect=new FileReader ("src/DATA/array.txt");
-			
+
 			br = new BufferedReader (lect);
 			try {
-				while(br.readLine() != null) {
-					b=br.readLine();
-
+				b=br.readLine();
+				while(b != null) {
 					String[] s1 = b.split(";");
-for(String a:s1) {
-	System.out.println(a);
-}
+					listC.add(new carro(s1[0],s1[1],s1[2],s1[3],s1[4],
+							s1[5],s1[6],Long.parseLong(s1[7]),Integer.parseInt(s1[8]),
+							Integer.parseInt(s1[9]),Double.parseDouble(s1[10]),
+							Integer.parseInt(s1[11]),Integer.parseInt(s1[12])));
 					//System.out.println(s1[0] + " " + s1[1]);
-					
+					b=br.readLine();
 				}
-			
+
 
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -169,9 +169,9 @@ for(String a:s1) {
 			}
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch 
-			System.out.println("malo");
+			System.out.println("SI ES LA PRIMERA VEZ QUE ABRE EL PROGRAMA POR FAVOR AÑADA UN AUTO PRIMERO");
 		}
-		
+
 
 	}
 
